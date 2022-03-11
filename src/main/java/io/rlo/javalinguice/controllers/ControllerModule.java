0e11fe3fce1f4fd51dde69cb7c9290bb7711c54d
Joseph Dicarlo <jbdicarlo@carilionclinic.org>
2022-03-11 11:54:34 -0500

    moved multibinder to static class to make binding multiple controllers easier

--- src/main/java/io/rlo/javalinguice/controllers/ControllerModule.java
+++ src/main/java/io/rlo/javalinguice/controllers/ControllerModule.java
@@ -1,14 +1,20 @@
 package io.rlo.javalinguice.controllers;
 
 import com.google.inject.AbstractModule;
+import com.google.inject.Binder;
 import com.google.inject.multibindings.Multibinder;
 import io.javalin.core.plugin.Plugin;
 
 public class ControllerModule extends AbstractModule {
+
+    private static void bindController(Binder binder, Class<? extends Plugin> pluginClass) {
+        Multibinder.newSetBinder(binder, Plugin.class)
+                .addBinding()
+                .to(pluginClass);
+    }
+
     @Override
     protected void configure() {
-        Multibinder.newSetBinder(binder(), Plugin.class)
-                .addBinding()
-                .to(MyController.class);
+        bindController(binder(), MyController.class);
     }
 }
